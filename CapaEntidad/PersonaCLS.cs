3003463bd86f079cba82ed6715084b225e235a76
Oscar200301 <21030082@itesa.edu.mx>
2025-04-04 14:33:17 -0600

    v1.1 - Primer commit prueba

---
+++ CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class PersonaCLS
+    {
+        //Listado
+
+        public int iidpersona { get; set; }
+        public string nombrecompleto { get; set; }
+        public string correo { get; set; }
+        public string fechanacimiento { get; set; }
+    }
+}
